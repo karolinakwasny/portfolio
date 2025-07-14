b298c40c3d50c04cd95824e34e9341d1eddc3aa9
karolinakwasny <kwasnykarolina98@gmail.com>
2025-07-14 18:16:39 +0200

    new dist

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,12 +5,12 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/portfolio/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/portfolio/" : "/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+}));
